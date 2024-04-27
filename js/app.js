4336e94d484caaf4e90fbcffc06a6cea51750289
Fabián Huertas <miloh9318@hotmail.com>
2024-04-26 20:28:11 -0500

    creando una clase para notificacion para mostrar mensajes de error

--- js/app.js
+++ js/app.js
@@ -37,7 +37,37 @@ function submitCita(e) {
     e.preventDefault();
 
     if( Object.values(citaObj).some(valor => valor.trim() === '')) {
-        console.log('todos los campos son obligatorios');
+        new Notificacion({
+            texto: 'todos los campos son obligatorios',
+            tipo: 'error'
+        })
         return
+    }
+}
+
+class Notificacion {
+    constructor({texto, tipo}) {
+        this.texto = texto
+        this.tipo = tipo
+
+        this.mostrar()
+    }
+
+    mostrar() {
+        // Crear la notificacion
+
+        const alerta = document.createElement('DIV')
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 
+        'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm')
+
+        // Si es de tipo error, agrega una clase
+
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500')
+    
+        // Mensaje error
+        alerta.textContent = this.texto
+
+        // Insertar en el DOM
+        formulario.parentElement.insertBefore(alerta, formulario)
     }
 }

13379945c57ca7435800d837ec87d6d191dfc333
Fabián Huertas <miloh9318@hotmail.com>
2024-04-26 21:19:46 -0500

    evitar alertas duplicadas

--- js/app.js
+++ js/app.js
@@ -60,8 +60,11 @@ class Notificacion {
         alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 
         'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm')
 
+        // Eliminar alertas duplicadas
+        const alertaPrevia = document.querySelector('.alert')
+            alertaPrevia?.remove();
+        
         // Si es de tipo error, agrega una clase
-
         this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500')
     
         // Mensaje error
@@ -69,5 +72,10 @@ class Notificacion {
 
         // Insertar en el DOM
         formulario.parentElement.insertBefore(alerta, formulario)
+
+        // Quitar despues de 3 seg
+        setInterval(() => {
+            alerta.remove()
+        }, 3000);
     }
 }

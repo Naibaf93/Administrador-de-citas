4d89cda2f67e5ff51a75424a591ba346faed61cd
Fabián Huertas <miloh9318@hotmail.com>
2024-04-28 18:12:16 -0500

    añadir la informacion al objeto de cita que deseamos editar

--- js/app.js
+++ js/app.js
@@ -111,9 +111,8 @@ class Admincitas {
             const btnEditar = document.createElement('button');
             btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
             btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
-            btnEditar.onclick = () => {
-                alert('diste click en editar...');
-            }
+            const clone = structuredClone(cita);
+            btnEditar.onclick = () => cargarEdicion(clone);
 
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
@@ -177,4 +176,8 @@ function reiniciarObjetoCita() {
         fecha : '',
         sintomas : '' 
     })
+}
+
+function cargarEdicion(cita) {
+    console.log(cita);
 }
